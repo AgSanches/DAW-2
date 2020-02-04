dc0afd69b8c116be74e0861df6c0bdc4a9d9b6a9
Adrián <adriangsanchez98@gmail.com>
2020-02-04 10:16:26 +0000

    Daw2 - Añadida tabla de Todo's con acción de acabar o no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'borra';
+  title = 'Todo List';
+  model = {
+    user: "Daw",
+    items: [
+      {
+        action:"Estudiar daw", done: false
+      } , {
+        action:"Estudiar 2", done: true
+      } , {
+        action:"Estudiar 3", done: false
+      },
+      {
+        action:"Estudiar 4", done: false
+      },
+    ]
+  };
 }
